e8230895902913113a5bfecdcc7437a7112a0405
“[Arshad <[Shaharshad57@gmail.com]>
2021-01-27 17:13:31 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,32 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numlives;
+
+    public int getlives()
+    {
+        return numlives;
+    }
+
+    public void setlives(int numlives)
+    {
+        this.numlives = numlives;
+    }
+
+    public void kill() {
+        if (numlives > 0){
+            numlives--;
+            System.out.println("Ouch!");
+        }
+        if(numlives == 0){
+            System.out.println("Dead");
+        }
+    }
+
     public Cat(String name)
     {
         super(name);
+        this.numlives = 9;
     }
+
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -4,26 +4,35 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        int i;
+        // System.out.println("Hello world");
 
-        Animal misty = new Dog("Misty");
+        // Animal misty = new Dog("Misty");
 
-        Animal topCat = new Cat("TopCat");
+        // Animal topCat = new Cat("TopCat");
 
-        System.out.println(misty);
-        System.out.println(topCat);
+        // System.out.println(misty);
+        // System.out.println(topCat);
 
-        misty = topCat;
+        // misty = topCat;
 
-        topCat.setName("Garfield");
+        // topCat.setName("Garfield");
 
-        System.out.println(misty);
-        System.out.println(topCat);
+        // System.out.println(misty);
+        // System.out.println(topCat);
 
-        // What will get printed out??
+        // // What will get printed out??
 
-        // 1. topcat, Garfield
-        // 2. garfield, Garfield 
+        // // 1. topcat, Garfield
+        // // 2. garfield, Garfield 
+
+        Cat ginger = new Cat("Ginger");
+        System.out.println(ginger);
+
+        int lives = ginger.getlives();
+        for (i = 0; i < lives; i++){
+            ginger.kill();
+        }
 
 
     }
